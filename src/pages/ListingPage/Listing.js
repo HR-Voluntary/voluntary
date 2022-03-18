e994c4e08de0a4157869c1a7ded913ebbd832827
Alexandria Norvani <acnorvani@gmail.com>
2022-03-18 15:01:32 -0700

    initial setup

---
+++ src/pages/ListingPage/Listing.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+const Listing = ({listing}) => {
+  return (
+    <div className="indiv-listing">
+      <img className="listing-image" src={listing.image} alt="">
+      <span className="listing-loc">{listing.location}</span>
+      <span className="listing-descp">{listing.description}</span>
+      <span className="seller-trust-score">{listing.seller.trustScore}</span>
+      <span className="seller-name">{listing.seller.name}</span>
+      <span className="listing-category">{listing.category}</span>
+    </div>
+  )
+};
+
+export default Listing;
--- src/pages/ListingPage/ListingPage.js
+++ src/pages/ListingPage/ListingPage.js
@@ -1,8 +1,23 @@
 import React from 'react';
+import axios from 'axios';
+import Listing from './Listing.js';
+
 
 const ListingPage = () => {
+  let [allListings, setAllListings] = useState([])
 
-  return <div>Listing Page</div>
+  const getListings = () => {
+    axios.get()
+  }
+
+  return (
+    <div>
+      <h1>Listing Page</h1>
+      {allListings.map(listing => {
+        return <Listing listing={listing}/>
+      })}
+    </div>
+  )
 };
 
 export default ListingPage;
