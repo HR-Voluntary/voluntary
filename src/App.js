cd377c0e685c114433f571b2977a1175a3eb67b8
Austin Beyaoju <Beyaoju@gmail.com>
2022-03-17 18:46:53 -0700

    Login page done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Login from "./components/Login";
 import './App.css';
 
 const App = () => {
   return (
     <div className="App">
-      <h1>hi</h1>
+      <Router>
+        <Routes>
+          <Route path='/' element={<Login />}/>
+        </Routes>
+      </Router>
     </div>
   );
 }
