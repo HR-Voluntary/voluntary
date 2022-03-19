73cbdf81631a4e1614f8edec3f38cda80642d34d
Rockwell Allen <rockwellallen@gmail.com>
2022-03-19 16:24:14 -0700

    main page functionality, working on other items

--- src/pages/ProductPage/ProductPage.js
+++ src/pages/ProductPage/ProductPage.js
@@ -1,48 +1,69 @@
 import React, { useState, useEffect } from 'react';
-const axios = require('axios');
+import axios from 'axios';
 import { logout } from '../../firebase';
+import styles from './ProductPage.module.css';
+import ProductPicView from './ProductPicView.jsx'
 
 // get product from Listings page
-
+// if they click get similar products or more from seller
+  // it will bring up a list of thos products in the place of the description
+// clicking on any image will bring up the prodcut display for that image
 
 const ProductPage = () => {
 
   const [pageDisplay, setPageDisplay] = useState('product')
-  const [products, setProducts] = useState([])
+  const [product, setProduct] = useState([])
+  const [mainImage, setMainImage] = useState('')
+  const [newProducts, setNewProdcuts] = useState({})
+
+  const dummyData = '5usff6HI0mIB2TTRy2Ut';
+
 
   function clickChat() {
 
   }
 
-  function renderImages() {
+  function clickImage(e) {
+    // alert('clicked');
+    console.log(e.target.src);
+    setMainImage(e.target.src);
+  }
 
+  function renderImages() {
+    return (
+      <>
+        {product.image.map(img => {
+          return <img src={img} className={styles.allProductImages} onClick={clickImage} alt="different views of item"/>;
+          })
+        }
+      </>
+    )
   }
 
   function renderPage() {
     if (pageDisplay === 'product') {
       return (
         <>
-          <img className= "product-img" src=""/>
-          {renderImages}
-          <h2>Product Name</h2>
-          <div>Description</div>
-          <button onClick={clickChat}>Chat with Seller</button>
+          <div className="main-product-pic-box">
+            <img className= "product-img" src={mainImage || product.image} alt="item"/>
+            <div>{product.image ? renderImages() : ''}</div>
+          </div>
+          <div className="other-info-box">
+            <h2>{product.name}</h2>
+            <div className={styles.description}>{product.description}</div>
+            <button onClick={clickChat}>Chat with Seller</button>
+          </div>
         </>
       )
     } else if (pageDisplay === 'similar') {
       return (
         <>
-          {products.map(product => {
-            return <div>image</div>}
-          )}
         </>
       )
-    } else if (pageDisplay === 'otherSellerItems') {
+    } else if (pageDisplay === 'sellerItems') {
       return (
         <>
-          {products.map(product => {
-            return <div>image</div>}
-          )}
+
         </>
       )
     } else {
@@ -50,18 +71,45 @@ const ProductPage = () => {
     }
   }
 
+  function clickSimilar() {
+    const category = product.category;
+    setPageDisplay('similar');
+    axios.get(`http://localhost:3000/category/${category}`)
+      .then(response => {
+        console.log('category response ', response)
+        // setNewProducts(response.data))
+      })
+      // .catch(err => console.log(err))
+  }
+
+  function clickSellerItems() {
+    const sellerId = product.sellerInfo;
+    setPageDisplay('sellerItems');
+    axios.get(`http://localhost:3000/${sellerId}`)
+      .then(response => {
+        console.log('category response ', response)
+        // setNewProducts(response.data))
+      })
+      // .catch(err => console.log(err))
+  }
+
   useEffect(() => {
-    axios.get(``) // limit the get of images to fit the screen
-      .then(response => response.json())
-      .then(data => setProducts(data))
-  }, [pageDisplay])
+    axios.get(`http://localhost:3000/item/${dummyData}`) // limit the get of images to fit the screen
+      .then(response => {
+        console.log(response)
+        setProduct(response.data)
+      })
+  }, [])
+
 
   return (
     <>
       {/* link back to listing page */}
       <button onClick={() => setPageDisplay('listings')}>Back to Listings</button>
-      <button onClick={() => setPageDisplay('similar')}>Similar Products</button>
-      <button onClick={() => setPageDisplay('otherSellerItems')}>Other Items from the Seller</button>
+      {/* <button onClick={() => setPageDisplay('similar')}>Similar Products</button> */}
+      <button onClick={clickSimilar}>Similar Products</button>
+      {/* <button onClick={() => setPageDisplay('sellerItems')}>Other Items from the Seller</button> */}
+      <button onClick={clickSellerItems}>Other Items from the Seller</button>
       <div>
         {renderPage()}
       </div>
@@ -71,3 +119,8 @@ const ProductPage = () => {
 }
 
 export default ProductPage;
+
+
+// {product.image.map((img, index) =>
+//   <ProductPicView prodImages={product.image} key={index}/>
+// )}
---
+++ src/pages/ProductPage/ProductPicView.jsx
@@ -0,0 +1,19 @@
+import React, { useState, useEffect } from 'react';
+
+const ProductPicView = () => {
+
+
+
+
+
+  
+  return (
+    <>
+      <div></div>
+    </>
+  )
+}
+
+
+
+export default ProductPicView;
