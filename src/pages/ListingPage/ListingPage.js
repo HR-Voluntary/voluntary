962a8bcfb1522f6437abd9636bc991290b855b9c
mtanaka <matthew.s.tanaka@gmail.com>
2022-03-19 14:56:27 -0700

    implement state and helper functions on ListingPage

--- src/pages/ListingPage/ListingPage.js
+++ src/pages/ListingPage/ListingPage.js
@@ -1,12 +1,51 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import MapListing from './Map/Map.js';
+import axios from 'axios';
+
 
 const ListingPage = () => {
+  let [allListings, setAllListings] = useState([]);
+  let [filterListing, setFilterListing] = useState([]);
+  // state signifying distance
+
+
+  useEffect(() => {
+    getListings()
+  }, [])
+
+  // useEffect(() => {
+    // save user location
+  // }, [])
+
+  const getListings = () => {
+    axios.get('http://localhost:3000/item/all')
+      .then(response => {
+        //calculate distance from current user
+        //sort by default distance
+        setAllListings(response.data)
+      })
+  }
+
+  const filterListings = (listings, filterParam) => {
+ //
+    //
+  }
+
+  // axios calls
+  // state/states with array of data
+  // filtering functions
+  // save user location- TBD
+  // onClick functions to navigate to individual product page
+
+
+
+  // function to calculate distance based on lat/long
 
   return (
     <>
       <div>Listing Page</div>
       <MapListing />
+      {/* pass in stat signifiying initial distance filter */}
     </>
   )
 
