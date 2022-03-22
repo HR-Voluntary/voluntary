8aa1b0fb0c01a9d7bcb9c7ce431cf1b014ccb575
sotoi <sotoi@uci.edu>
2022-03-21 21:31:32 -0700

    fixed some rendering bugs

--- src/pages/ChatPage/dummydata.js
+++ src/pages/ChatPage/dummydata.js
@@ -1,13 +1,13 @@
 
-export const usera = {
+export const userk = {
   authProvider:'local',
   email:'test2@test2.com',
-  uid: 'Yk1yHxvkbVcM93wq8UGMbkNGhVf1',
+  uid: 'YYr7QsRYHRawcKrsnoICvhGbtok1',
   name:"irving"
 }
 export const userb = {
   authProvider:'google',
   email:'aubeyaoju@gmail.com',
-  uid: '1AOjnwnoc5bxD1u3VBiaNzKYL2k1',
+  uid: 'YYr7QsRYHRawcKrsnoICvhGbtok1',
   name:"Austin"
 }
