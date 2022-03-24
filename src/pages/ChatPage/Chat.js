1dabac3ee294b7bbe9720dc7a20e5bdb27db158e
sotoi <sotoi@uci.edu>
2022-03-24 16:36:42 -0700

    finished chat not touching it ever again complete feature set no bugs

--- src/pages/ChatPage/Chat.js
+++ src/pages/ChatPage/Chat.js
@@ -8,6 +8,7 @@ import {
   orderBy,
   query,
   getDoc,
+  getDocs,
   updateDoc,
   addDoc,
   setDoc,
@@ -42,6 +43,8 @@ function Chat({ changeUser,user1, user2, userList, product, productId }) {
   },[modal])
 
   useEffect(()=>{
+
+    // debugger
     if (Object.keys(user1).length && Object.keys(user2).length) {
       const roomId =
         user1.uid > user2.uid
@@ -49,10 +52,11 @@ function Chat({ changeUser,user1, user2, userList, product, productId }) {
           : `${user2.uid + user1.uid}`;
       getDoc(doc(db, 'conversations', user1?.uid, 'to', user2?.uid)).then(
         (res) => {
+          console.log(res.data())
           setItemId(res.data()?.item);
         }
       );
-    } else{console.log(item)
+    } else{
       if(productId){
 
         setItemId(productId)
@@ -65,9 +69,8 @@ function Chat({ changeUser,user1, user2, userList, product, productId }) {
   useEffect(() => {
 
     if (itemId) {
-
       let unsubscribe = onSnapshot(doc(db, 'items', itemId), (doc) => {
-        console.log('setItem fired',doc.data())
+
         setItem(doc.data());
       });
       return unsubscribe;
@@ -78,6 +81,7 @@ function Chat({ changeUser,user1, user2, userList, product, productId }) {
       return unsubscribe
     }
   }, [itemId]);
+
   useEffect(() => {
 
     const roomId =
@@ -126,8 +130,12 @@ function Chat({ changeUser,user1, user2, userList, product, productId }) {
 
   async function sendMessage(e) {
     e.preventDefault();
-    addMessageToConversation(messageText);
+    if(messageText.length){
+      addMessageToConversation(messageText);
     setMessageText('');
+
+    }
+
   }
 
   let onSellerClick = async (item) => {
@@ -140,9 +148,19 @@ function Chat({ changeUser,user1, user2, userList, product, productId }) {
     //console.log('ABOUT TO RENDER MODAL');
   }
   let deleteConversations = async () =>{
+    let results=[]
     await deleteDoc(doc(db, 'conversations', user1?.uid, 'to', user2?.uid))
-   await deleteDoc(doc(db, 'conversations', user2?.uid, 'to', user1?.uid))
-  }
+    getDocs(collection(db, "conversations",user2?.uid,'to')).then((qSnap)=>{
+      qSnap.forEach((docu) => {
+        if(docu?.data()?.item === itemId){
+          return results.push(deleteDoc(doc(db,'conversations',user2.uid,'to',docu?.data()?.id)))
+        }
+    });
+
+  })
+  Promise.all(results);
+}
+
 
   let onBuyerClick = async () => {
     if(itemId){
@@ -175,11 +193,17 @@ function Chat({ changeUser,user1, user2, userList, product, productId }) {
   };
   return (
     <div className={styles.messageContainer}>
+      {/* {(getDoc(doc(db, 'conversations', user1?.uid)).then(
+        (res) => {
+          console.log(res.data())
+
+        }
+      ))} */}
       <div className={styles.topBar}>
       <div className={styles.statusName}>
       {userList.find((user)=>{if(user.uid===user2.uid){return user.uid}})?.active?<div className={styles.green}></div>:<div className={styles.red}></div>}
       <div className={styles.displayName}>{user2?.name}</div>
-      {item?.buyer===user1?.uid|| item?.sellerInfo===user2?.uid?<div>{item.name}</div>:<div></div>}
+      {item?.sellerInfo===user2?.uid || item?.sellerInfo===user1?.uid?<div className={styles.itemName}>{item.name}</div>:<div></div>}
       </div>
       {renderButton()}
 
@@ -194,7 +218,7 @@ function Chat({ changeUser,user1, user2, userList, product, productId }) {
           value={messageText}
           onChange={onChangeTextHandler}
         />
-        <input type='submit' value='submit'></input>
+        {/* <input type='submit' value='submit'></input> */}
       </form>
 
       {modal ? <ReviewsModal uid={user2.uid} type={userType}/> : <></>}
